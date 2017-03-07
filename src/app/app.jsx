a853f73f4d9dcbda2d93235ddcc2df651022fab8
rachel <rachelcantor@gmail.com>
2017-03-06 21:05:27 -0500

    dunno

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,14 +1,26 @@
-(function () {
-    require('uswds');
-    var React = require('react'),
-        ReactDOM = require('react-dom'),
-        Main = require('./components/main.jsx');
+var app = app || {};
+(function() {
+	require('uswds');
+	var React = require('react');
+	var ReactDOM = require('react-dom');
 
-    // Needed for React Developer Tools ?
-    // window.React = React;
+	var HouseholdApp = require('./components/householdApp.jsx');
+	var HouseholdData = require('./components/householdData.jsx');
+	// Main = require('./components/main.jsx');;
 
-    // Render the main app react component into the document body.
-    // https://facebook.github.io/react/blog/2015/10/01/react-render-and-top-level-api.html
-    ReactDOM.render(<Main />, document.getElementById("react-target"));
+	// Needed for React Developer Tools ?
+	// window.React = React;
+
+	// Render the main app react component into the document body.
+	// https://facebook.github.io/react/blog/2015/10/01/react-render-and-top-level-api.html
+
+	var model = new HouseholdData("currentHousehold");
+	function render() {
+		ReactDOM.render(
+			<HouseholdApp model={model}/>, document.getElementById("react-target"));
+	}
+
+	model.subscribe(render);
+	render();
 
 })();
---
+++ src/app/components/householdApp.jsx
@@ -0,0 +1,166 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Bootstrap = require('react-bootstrap');
+
+// const Grid = Bootstrap.Grid, Row = Bootstrap.Row, Col = Bootstrap.Col;
+// const Panel = Bootstrap.Panel;
+const Table = require('react-bootstrap').Table;
+const Button = require('react-bootstrap').Button;
+const FormGroup = require('react-bootstrap').FormGroup;
+const Modal = require('react-bootstrap').Modal;
+const OverlayTrigger = require('react-bootstrap').OverlayTrigger;
+var Person = require('./person.jsx');
+
+let persons = [];
+// app.ALL_PERSONS = 'all';
+// var TodoFooter = app.TodoFooter;
+
+var ENTER_KEY = 13;
+
+var AddPersonModal = React.createClass({
+	getInitialState: function() {
+		console.log('AddPersonModal', 'getInitialState');
+		console.log(this);
+		return {showModal: false};
+	},
+	close: function() {
+		this.setState({showModal: false});
+	},
+	open: function() {
+		this.setState({showModal: true});
+		if (document.getElementById("name") && document.getElementById("description") && document.getElementById("fruit")) {
+			$("#name").val("");
+			$("#description").val("");
+			$("#fruit").val("");
+		} else {
+			requestAnimationFrame(this.open);
+		}
+	},
+	add: function() {
+		let name = document.getElementById("name").value;
+		let description = document.getElementById("description").value;
+		let fruit = document.getElementById("fruit").value;
+		if (name.length < 1)
+			name = "Unknown";
+		console.log('add it');
+		// household.push({name: name, description: description, fruit: fruit});
+		// Household.update();
+		this.close();
+	},
+	render: function() {
+		return (
+			<div>
+				<Button bsClass="usa-button" onClick={this.addNewPerson} id="show">
+					Add Person
+				</Button>
+				<Modal bsSize="small" show={this.state.showModal} onHide={this.closeModal} aria-labelledby="modalTitle">
+					<Modal.Header closeButton>
+						<Modal.Title id="modalTitle">Add a Person</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<form>
+							<FormGroup id="name" type="text" label="Name" placeholder="Enter name"/>
+							<FormGroup id="description" type="text" label="Description" placeholder="Enter description"/>
+							<FormGroup id="fruit" type="text" label="Favorite Fruit" placeholder="Enter favorite fruit"/>
+						</form>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button onClick={this.addNewPerson} bsClass="usa-button" id="add">Add Person to Household</Button>
+						<Button onClick={this.closeModal} bsClass="usa-button usa-button-gray">Close</Button>
+					</Modal.Footer>
+				</Modal>
+			</div>
+		)
+	}
+});
+
+var HouseholdApp = React.createClass({
+	getInitialState: function() {
+		console.log('HouseholdApp', 'getInitialState');
+		return {nowShowing: 'app.ALL_PERSONS', editing: null, newPerson: ''};
+	},
+
+	componentDidMount: function() {
+		console.log('HouseholdApp', 'componentDidMount');
+		var setState = this.setState;
+	},
+
+	handleChange: function(event) {
+		console.log('HouseholdApp', 'handleChange');
+		this.setState({newPerson: event.target.value});
+	},
+
+	addNewPerson: function() {
+		console.log('HouseholdApp', 'addNewPerson');
+
+	},
+
+	handleNewPersonKeyDown: function(event) {
+		console.log('HouseholdApp', 'handleNewPersonKeyDown');
+		if (event.keyCode !== ENTER_KEY) {
+			return;
+		}
+
+		event.preventDefault();
+
+		var val = this.state.newPerson.trim();
+
+		if (val) {
+			console.log('HouseholdApp', 'if val');
+			this.props.model.addPerson(val);
+			this.setState({newPerson: ''});
+		}
+	},
+
+	destroy: function(person) {
+		console.log('HouseholdApp', 'destroy');
+		this.props.model.destroy(person);
+	},
+
+	edit: function(person) {
+		console.log('HouseholdApp', 'edit');
+		this.setState({editing: person.id});
+	},
+
+	save: function(personToSave, text) {
+		console.log('HouseholdApp', 'save');
+		this.props.model.save(personToSave, text);
+		this.setState({editing: null});
+	},
+
+	cancel: function() {
+		console.log('HouseholdApp', 'cancel');
+		this.setState({editing: null});
+	},
+
+	render: function() {
+		var main;
+		var householdPersons = this.props.model.householdPersons;
+
+		var persons = householdPersons.map(function(person) {
+			return (<Person key={person.id} person={person} onDestroy={this.destroy.bind(this, person)} name={person.name} onSave={this.save.bind(this, person)} onCancel={this.cancel}/>);
+		}, this);
+
+		return (
+			<div>
+				<Table bsClass="usa-table-borderless">
+					<thead>
+						<tr>
+							<th scope="col">Full Name</th>
+							<th scope="col">Description</th>
+							<th scope="col">Favorite Fruit</th>
+							<th scope="col">Del</th>
+						</tr>
+					</thead>
+					<tbody>
+						{persons}
+					</tbody>
+				</Table>
+				<input className="new-todo" placeholder="What needs to be done?" value={this.state.newPerson} onKeyDown={this.handleNewPersonKeyDown} onChange={this.handleChange} autoFocus={true}/>
+				<AddPersonModal/>
+			</div>
+		);
+	}
+});
+
+module.exports = HouseholdApp;
